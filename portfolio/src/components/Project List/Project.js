f41a4cddbc2b86a288c20855b50864763fe59fe6
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-10-16 18:39:16 -0500

    more slight style changes

--- portfolio/src/components/Project List/Project.js
+++ portfolio/src/components/Project List/Project.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 const Project = (props) => {
-    const {img, description, name} = props.project
+    const { img, description, name } = props.project
     return (
 
         <div className="item">
-            <img src={img}/>
+            <img src={img} />
             <div className="info">
                 <h2>{name}</h2>
                 <p>{description}</p>
+                <button>> &nbsp; &nbsp;Learn More</button>
             </div>
 
         </div>
