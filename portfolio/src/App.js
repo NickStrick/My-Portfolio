f439c5f7ca1b035ff62cde352d8f1f98e9940981
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-10-31 16:47:04 -0500

    changed where project data is stored

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,29 +6,31 @@ import ProjectList from './components/Project List/ProjectList.js';
 import Navbar from './components/navbar/Navbar.js';
 import Contactinfo from './components/ContactInfo/Contactinfo.js';
 
+const pData = [
+  {
+    name: "Project one",
+    description: 'now this was a project that was really fun to do! i worked on it with a team of a few people , go take a look!',
+    img: logo
+  },
+  {
+    name: "Project two",
+    description: 'now this was a project that was really fun to do! i worked on it with a team of a few people , go take a look!',
+    img: logo
+  },
+  {
+    name: "Project three",
+    description: 'now this was a project that was really fun to do! i worked on it with a team of a few people , go take a look!',
+    img: logo
+  },
+  {
+    name: "Project four",
+    description: 'now this was a project that was really fun to do! i worked on it with a team of a few people , go take a look!',
+    img: logo
+  },
+]
+
 function App() {
-  const [projects] = useState([
-    {
-      name: "Project one",
-      description: 'now this was a project that was really fun to do! i worked on it with a team of a few people , go take a look!',
-      img: logo
-    },
-    {
-      name: "Project two",
-      description: 'now this was a project that was really fun to do! i worked on it with a team of a few people , go take a look!',
-      img: logo
-    },
-    {
-      name: "Project three",
-      description: 'now this was a project that was really fun to do! i worked on it with a team of a few people , go take a look!',
-      img: logo
-    },
-    {
-      name: "Project four",
-      description: 'now this was a project that was really fun to do! i worked on it with a team of a few people , go take a look!',
-      img: logo
-    },
-  ])
+  const [projects] = useState(pData)
 
   return (
     <div className="App">
