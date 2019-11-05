614a63059f03918eed039e8035170fff89fb0faa
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-11-04 19:50:47 -0600

    added featured project component

---
+++ portfolio/src/components/featured/FeaturedProject.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Project = (props) => {
+    if (!(props.project.name)) {
+        props.project.name = 'ERROR'
+    }
+    const { img, description, name } = props.project
+
+    return (
+
+        <div className="item">
+            <img src={img} />
+            <div className="info">
+                <h2 className='title'>{name}</h2>
+                <p aria-label={'project-description'}>{description}</p>
+                <button>> &nbsp; &nbsp;Learn More</button>
+            </div>
+
+        </div>
+    );
+}
+
+
+export default Project;
