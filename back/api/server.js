845d28960e73a16ae179e41a2460a6bd5c1b2551
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-11-06 23:26:28 -0600

    started server file for backend

---
+++ back/api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const server = express();
+
+
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+    res.status(200).json({ Title: 'Mympy Server Up!' });
+});
+
+
+module.exports = server;
