bfa4207f6d56db0be14dc758d2451f548de8d22f
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-11-12 14:40:39 -0600

    added inital images, titles, and links to projects

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
+
 import logo from './logo.svg';
 import riyLogo from './images/RIY.png'
+import mympyLogo from './images/Mympy.png'
+import luncherLogo from './images/luncherApp.png'
+import sandjLogo from './images/sandj.png'
+import notesLogo from './images/lambdaNotes.png'
+
 import './App.scss';
 import Home from './components/Home.js';
 import ProjectList from './components/Project List/ProjectList.js';
@@ -12,25 +18,35 @@ const pData = [
     name: "Review It Yourself",
     description: 'now this was a project that was really fun to do! i worked on it with a team of a few people , go take a look!',
     img: riyLogo,
-    link: 'https://how-tutor.netlify.com/'
+    link: 'https://github.com/labs13-how-to',
+    deployed: 'https://how-tutor.netlify.com/'
   },
   {
-    name: "Project two",
+    name: "Mympy Dreams",
     description: 'now this was a project that was really fun to do! i worked on it with a team of a few people , go take a look!',
-    img: logo,
-    link: 'https://github.com/NickStrick?tab=repositories'
+    img: mympyLogo,
+    link: 'https://github.com/mympy-dreamers',
+    deployed: 'https://mympydreamers.netlify.com/'
   },
   {
-    name: "Project three",
+    name: "Luncher App",
     description: 'now this was a project that was really fun to do! i worked on it with a team of a few people , go take a look!',
-    img: logo,
-    link: 'https://github.com/NickStrick?tab=repositories'
+    img: luncherLogo,
+    link: 'https://github.com/luncher-team/LA-Backend-Nick-Stricker',
+    deployed: 'https://luncherappltrii.netlify.com/'
   },
   {
-    name: "Project four",
+    name: "Lambda Notes",
     description: 'now this was a project that was really fun to do! i worked on it with a team of a few people , go take a look!',
-    img: logo,
-    link: 'https://github.com/NickStrick?tab=repositories'
+    img: notesLogo,
+    link: 'https://github.com/NickStrick/front-end-project-week',
+    deployed: 'https://nstrick-lambda-notes.netlify.com/'
+  },
+  {
+    name: "Smith and Jones Architects",
+    description: 'now this was a project that was really fun to do! i worked on it with a team of a few people , go take a look!',
+    img: sandjLogo,
+    link: 'https://github.com/NickStrick/User-Interface-Project-Week',
   },
 ]
 
