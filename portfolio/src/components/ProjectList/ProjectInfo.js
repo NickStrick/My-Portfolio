11249fed62359449a4ddd578b9ab909928295df9
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-10 17:44:02 -0600

    fleshed out main parts of info component

--- portfolio/src/components/ProjectList/ProjectInfo.js
+++ portfolio/src/components/ProjectList/ProjectInfo.js
@@ -6,12 +6,26 @@ const Project = (props) => {
     if (!(project.name)) {
         project.name = 'ERROR'
     }
-    const { img, description, name, link } = project
+    const { img, description, name, link, techUsed, teamMemebers, weeksCompleted, contribution, role } = project
 
     return (
 
         <div className="ProjectInfo">
-            <h1>{name}</h1>
+            <img src={img} alt={`${name} pic`} />
+            <h1 className='title'>{name}</h1>
+            <p aria-label={'project-description'}>{description}</p>
+            <p className='contribution'>{contribution}</p>
+            <ul>
+                {techUsed.map((tech, index) => <li key={index}>{tech}</li>)}
+            </ul>
+            <div>
+                <p>weeks worked on project: {weeksCompleted}</p>
+                <p>Team Size: {teamMemebers}</p>
+
+            </div>
+            <div className='project-links'>
+                <img src={gitLogo} target="_blank" onClick={() => window.open(link)} />
+                {project.deployed && <button target="_blank" onClick={() => window.open(project.deployed)}>Site</button>}</div>
 
         </div>
     );
