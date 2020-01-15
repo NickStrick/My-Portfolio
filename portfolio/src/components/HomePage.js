200cf5b7503f6d01287af562f19ecaefbcf33a15
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-14 20:21:16 -0600

    added some html changes

--- portfolio/src/components/HomePage.js
+++ portfolio/src/components/HomePage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Home from './Home.js';
 import ProjectList from './ProjectList/ProjectList.js';
 import Contactinfo from './ContactInfo/Contactinfo.js';
+import SkillsSection from './skills/SkillsSection.js';
 
 const HomePage = (props) => {
 
@@ -10,7 +11,10 @@ const HomePage = (props) => {
 
         <React.Fragment>
             <Home />
+            <div id='portfolio'></div>
             <ProjectList projects={props.projects} history={props.history} />
+            <div id='skills'></div>
+            <SkillsSection />
             <Contactinfo />
         </React.Fragment>
     );
