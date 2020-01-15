aafa98765cac000e9800c1d5e3959ebe8751b332
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-15 16:37:58 -0600

    added profile and homepage styling and reorganization

--- portfolio/src/components/HomePage.js
+++ portfolio/src/components/HomePage.js
@@ -5,17 +5,24 @@ import ProjectList from './ProjectList/ProjectList.js';
 import Contactinfo from './ContactInfo/Contactinfo.js';
 import SkillsSection from './skills/SkillsSection.js';
 
+import Pfp from '../images/pfp.jpg';
+
 const HomePage = (props) => {
 
     return (
-
         <React.Fragment>
-            <Home />
-            <div id='portfolio'></div>
-            <ProjectList projects={props.projects} history={props.history} />
-            <div id='skills'></div>
-            <SkillsSection />
-            <Contactinfo />
+            <div className='pfp-background'>
+                <img id='pfp' src={Pfp} alt='Nick S profile picture' />
+            </div>
+
+            <div className="container home-container">
+                <Home />
+                <div id='portfolio'></div>
+                <ProjectList projects={props.projects} history={props.history} />
+                <div id='skills'></div>
+                <SkillsSection />
+                <Contactinfo />
+            </div>
         </React.Fragment>
     );
 
