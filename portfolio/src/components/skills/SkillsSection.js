ca3781285b1c30cedbc5314c143a8b9a4fdc7349
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-15 13:12:59 -0600

    imported new skill images

--- portfolio/src/components/skills/SkillsSection.js
+++ portfolio/src/components/skills/SkillsSection.js
@@ -1,22 +1,33 @@
 
 import './SkillsSection.scss';
 import React, { useState } from 'react';
-import ReactLogo from '../../images/skills/logo.svg';
 import SkillItem from './SkillItem.js';
+
+import ReactLogo from '../../images/skills/logo.svg';
+import htmlLogo from '../../images/skills/HTML5.png';
+import cssLogo from '../../images/skills/CSS3.png';
+import JSLogo from '../../images/skills/JS.png';
+import nodeLogo from '../../images/skills/nodejs-logo.png';
+import gitLogo from '../../images/skills/git.png';
+import reduxLogo from '../../images/skills/redux.png';
+import SQL from '../../images/skills/SQL.png';
+import postgresLogo from '../../images/skills/postgres.png';
+import pythonLogo from '../../images/skills/python.png';
+
 
 
 const SkillsSection = () => {
     const skillList = [
-        { skill: 'HTML5', logo: ReactLogo },
-        { skill: 'CSS3', logo: ReactLogo },
-        { skill: 'JavaScript', logo: ReactLogo },
-        { skill: 'NodeJS', logo: ReactLogo },
+        { skill: 'HTML5', logo: htmlLogo },
+        { skill: 'CSS3', logo: cssLogo },
+        { skill: 'JavaScript', logo: JSLogo },
+        { skill: 'NodeJS', logo: nodeLogo },
         { skill: 'ReactJS', logo: ReactLogo },
-        { skill: 'Redux', logo: ReactLogo },
-        { skill: 'Python', logo: ReactLogo },
-        { skill: 'SQL', logo: ReactLogo },
-        { skill: 'Git', logo: ReactLogo },
-        { skill: 'Postgres', logo: ReactLogo }
+        { skill: 'Redux', logo: reduxLogo },
+        { skill: 'Python', logo: pythonLogo },
+        { skill: 'SQL', logo: SQL },
+        { skill: 'Git', logo: gitLogo },
+        { skill: 'Postgres', logo: postgresLogo }
     ]
     console.log(skillList[0])
 
@@ -24,8 +35,8 @@ const SkillsSection = () => {
         <div className="skills">
             <h1 className='skills-head'>Skills</h1>
             <div className='skill-list'>
-                {skillList.map((item) => {
-                    return <SkillItem item={item} />
+                {skillList.map((item, index) => {
+                    return <SkillItem item={item} key={index} id={`${item.skill}Logo`} />
                 })}
             </div>
 
