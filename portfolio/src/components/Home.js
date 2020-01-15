409b5586346e23e7e326cb4a845b085b6001c586
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-15 16:37:03 -0600

    fixed some spelling

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -22,11 +22,11 @@ const Home = () => {
 
 
     const summary1 = 'Hi, I\'m Nick. I\'m a full stack web developer that loves problem sovling and exploring the world of software.'
-    const summary2 = 'I spent the last couple of years learning and teaching to code. After highschool, curious about coding, i spent free time creating small games, and looking for resources to learn more and finding out what i could create while i worked in retail. I followed small courses on coding through things such as youtube, udemy, or watch and code.  I spent this past year enrolled in lambda school as a student and as a contract worker. During my time as a student, i learned Full Stack web development along with an assortment of librarys and frameworks, and some computer science fundamentals. With that i created some pretty cool projects that im proud of. I was a contract worker for lambda school as well, where i frist worked as a Team lead above students, leading daily meetings, sovling any debugging issues, helping my students in whatever way possible. The times i was working on a project with a team, i was responsible with the relationship between the stakeholder and the team, and managed our planning. i also debugged and filled any gaps in the conribution to the projects where nessesary. I spent most of my time in meetings getting to know my students and solving problems or planning with them.';
-    const summary3 = 'These exerpiences only feed my hunger for learning and solving puzzles, and they fuel me to be where i am today.';
+    const summary2 = 'I spent the last couple of years learning and teaching to code. After highschool, curious about coding, I spent free time creating small games, and looking for resources to learn more and finding out what I could create while I worked in retail. I followed small courses on coding through things such as Youtube, Udemy, or Watch and Code. I spent this past year enrolled in Lambda School as a student and as a contract worker. During my time as a student, I learned Full Stack web development along with an assortment of librarys and frameworks, and some computer science fundamentals. With that I created some pretty cool projects that I’m proud of. I was a contract worker for Lambda School as well, where I frist worked as a Team lead above students, leading daily meetings, sovling any debugging issues, helping my students in whatever way possible. The times I was working on a project with a team, I was responsible with the relationship between the stakeholder and the team, and managed our planning. I also debugged and filled any gaps in the conribution to the projects where nessesary. I spent most of my time in meetings getting to know my students and solving problems or planning with them.';
+    const summary3 = 'These exerpiences only feed my hunger for learning and solving puzzles, and they fuel me to be where I am today.';
     return (
         <header className="App-header" aria-label={'page-header'}>
-            <img id='pfp' src={Pfp} alt='Nick S profile picture' />
+            {/* <img id='pfp' src={Pfp} alt='Nick S profile picture' /> */}
             <div className='header-title'>
 
                 <h1>Nick Stricker</h1>
@@ -38,7 +38,7 @@ const Home = () => {
             </div>
 
             <p id='moto'>Electricity in the spirit, puzzles on the mind, code in my veins.</p>
-            <button className={sumBtn} onClick={toggleSum}>{sumOpen ? '⮙' : '⮛'}&nbsp; &nbsp; {sumBtn == 'sumBtn sumBtnClose' && 'read more'}</button>
+            <button className={sumBtn} onClick={toggleSum}>{sumOpen ? '⮙' : '⮛'}&nbsp; &nbsp; <span>{sumBtn == 'sumBtn sumBtnClose' && 'read more'}</span></button>
 
             <div className={`summary ${sumOpen ? 'open' : ''}`} >
                 <p className='summary-item'>{summary1}</p>
