699183d7da81a458b5c49b7d6eb77f140eb8e7bd
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-16 12:38:38 -0600

    new functionality for mail link in footer

--- portfolio/src/components/footer/Footer.js
+++ portfolio/src/components/footer/Footer.js
@@ -7,14 +7,45 @@ import mail from '../../images/mail-100.png'
 
 
 const Footer = (props) => {
+    const [showMail, setMail] = useState(false)
 
+    function myFunction() {
+        var copyText = document.getElementById("mailInput");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+
+        var tooltip = document.getElementById("myTooltip");
+        tooltip.innerHTML = "Copied: " + copyText.value;
+    }
+
+    function outFunc() {
+        var tooltip = document.getElementById("myTooltip");
+        tooltip.innerHTML = "Copy to clipboard";
+    }
 
     return (
 
         <div className="footer">
             <img src={gitLogo} className='gitLogo git' target="_blank" onClick={() => window.open('https://github.com/NickStrick')} />
             <img src={linkedin} className='gitLogo linkedin' target="_blank" onClick={() => window.open('https://www.linkedin.com/in/nick-stricker-1ba8a7192/')} />
-            <img src={mail} className='gitLogo mail' target="_blank" onClick={() => window.open('strickerdev@gmail.com')} />
+            <div className='mail-div'>
+                <img src={mail} className='gitLogo mail' target="_blank" onClick={() => setMail(!showMail)} />
+                <div className='mail-text'>
+                    {showMail &&
+                        <div className="tooltip">
+                            <button onClick={myFunction} onMouseOut={outFunc}>
+                                <span className="tooltiptext" id="myTooltip">Copy to clipboard</span>
+                                Copy text
+                        </button>
+                        </div>
+                    }
+                    {/* {showMail && <p id='mailInput'>strickerdev@gmail.com</p>} */}
+                    {showMail && <input type="text" id='mailInput' value='strickerdev@gmail.com' readOnly={true} />}
+
+                </div>
+            </div>
+
 
 
         </div >
