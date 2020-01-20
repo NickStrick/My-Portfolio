030496f36b00565a4982d056968167ceeb0c77d5
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-20 11:56:14 -0600

    new html

--- portfolio/src/components/ContactInfo/Contactinfo.js
+++ portfolio/src/components/ContactInfo/Contactinfo.js
@@ -8,6 +8,28 @@ const Contactinfo = (props) => {
     const [email, setEmail] = useState('')
     const [msg, setMsg] = useState('')
 
+    const [getTex, setTex] = useState(0)
+    // console.log('getTex: ', getTex)
+
+    // useEffect(() => {
+    //     const myTex = document.getElementById('contact-msg')
+    //     myTex.addEventListener('keypress', function (e) {
+    //         // console.log(e.target, e.key);
+    //         texSetNewLine(e.target, e.key)
+
+    //     })
+    // }, [])
+
+    // function texSetNewLine(tex, key) {
+    //     if (key && key === 'Enter') {
+    //         console.log('add line')
+    //     }
+    //     if (tex.value.length > 80 && tex.value.length % 82 == 0) {
+    //         console.log('add line')
+    //     }
+
+    // }
+
     function handleChange(e) {
         switch (e.target.name) {
             case 'firstName':
@@ -24,9 +46,24 @@ const Contactinfo = (props) => {
                 break;
             case 'msg':
                 setMsg(e.target.value)
+                // SetNewSize(e.target)
                 break;
         }
     }
+
+    // function SetNewSize(textarea) {
+    //     // console.log(textarea)
+    //     // console.log('getTex: ', getTex)
+    //     if (textarea.value.length)
+    //     setTex(textarea.value.length)
+
+    //     if (textarea.rows <= 20) {
+    //         textarea.rows += 1;
+    //     } else {
+    //         // textarea.cols = 10;
+    //         // textarea.rows = 15;
+    //     }
+    // }
 
     return (
 
@@ -66,6 +103,7 @@ const Contactinfo = (props) => {
                     />
                 </div>
                 <textarea
+                    id='contact-msg'
                     aria-label={'contact-msg'}
                     value={msg}
                     onChange={handleChange}
