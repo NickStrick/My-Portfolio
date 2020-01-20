74e74fe90fa221d3583ba0af7be0348ef263f7dc
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2020-01-20 11:56:28 -0600

    project list new html

--- portfolio/src/components/ProjectList/ProjectList.js
+++ portfolio/src/components/ProjectList/ProjectList.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Project from './Project.js';
+// let sWidth = screen.width;
 
 const ProjectList = (props) => {
+    let sWidth = window.screen.width;
     let middle = props.projects.length % 2 !== 0 ? ((props.projects.length + 1) / 2) : props.projects.length / 2;
+
     return (
         <>
 
@@ -10,6 +13,12 @@ const ProjectList = (props) => {
             <div className="projects" >
                 <h1 className='port-head'>Portfolio</h1>
                 <div className='projects-columns'>
+                    {/* <div className='project-list'>
+                        {props.projects.map((project, index) => {
+                            return <Project project={project} key={index} index={index} history={props.history} />
+
+                        }
+                        )}</div> */}
                     <div className='project-list'>
                         {props.projects.map((project, index) => {
 
