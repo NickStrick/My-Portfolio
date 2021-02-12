4109a096cc1052859bb67320d5500269832be728
Nick Stricker <strickerdev@gmail.com>
2021-02-12 16:20:08 -0600

    fontawesome and css imports added

---
+++ portfolio/src/utilities/Fontawesome.js
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCaretDown, faCaretUp, faChevronUp, faChevronDown} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCaretDown, faCaretUp, faChevronUp, faChevronDown);
